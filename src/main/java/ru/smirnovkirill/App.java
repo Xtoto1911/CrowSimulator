79725fbdef740f0ab2ee804522386acd9ed79bd0
Kirill Smirnov <Xtot0ti@yandex.ru>
2025-12-03 01:14:09 +0300

    test

--- src/main/java/ru/smirnovkirill/App.java
+++ src/main/java/ru/smirnovkirill/App.java
@@ -1,13 +1,8 @@
 package ru.smirnovkirill;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Hi");
     }
 }
